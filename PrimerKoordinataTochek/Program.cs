5ff448133b993f4f8ef66b2e84183f2b582598c3
Roman Pushnikov <r.pushnikov@mail.ru>
2023-02-08 22:45:45 +0300

    first

---
+++ PrimerKoordinataTochek/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введиет координату x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введиет координату y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0 ) Console.WriteLine("1 quater");
+else if (x < 0 && y > 0 ) Console.WriteLine("2 quater");
+else if (x < 0 && y < 0 ) Console.WriteLine("3 quater");
+else if (x > 0 && y < 0 ) Console.WriteLine("4 quater");
